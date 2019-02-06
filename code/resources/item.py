03506aaf5c317d19b0844ce106359e1a2735fe5c
rifkegribenes <rifkegribenes@gmail.com>
2019-02-05 21:49:37 -0800

    itemlist resource

--- code/resources/item.py
+++ code/resources/item.py
@@ -62,4 +62,4 @@ class Item(Resource):
 
 class ItemList(Resource):
 	def get(self):
-		return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
+		return {'items': [item.json() for item in ItemModel.query.all()]}
